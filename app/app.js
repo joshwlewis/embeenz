5c6552a4e4ad50a88ea8e7c508b5738c2e6b13eb
Josh W Lewis <josh.w.lewis@gmail.com>
2015-11-01 21:46:03 -0600

    First pass

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
@@ -7,10 +8,15 @@ var App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
+const Adapter = DS.RESTAdapter.extend({
+  host: 'https://beenz.herokuapp.com'
+});
+
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver: Resolver,
+  ApplicationAdapter: Adapter
 });
 
 loadInitializers(App, config.modulePrefix);
---
+++ app/components/beenz-kitties.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ["kitties"],
+
+  kitties: Ember.computed('beenz', function() {
+    return [1,2,3,4,5].map((been) => {
+      return {
+        image: this.getImage(been),
+        been:  been
+      };
+    });
+  }),
+
+  getImage(been) {
+    const beenz = this.get('beenz');
+    if (beenz >= been) {
+      if (this.get('multicolor')) {
+        return been;
+      } else {
+        return beenz;
+      }
+    } else {
+      return 0;
+    }
+  }
+});
